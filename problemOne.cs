bf9ccb81cb2384032d251edcfe1757d515f944a9
mahamdeh0 <mahamdehabd@gmail.com>
2024-09-05 20:23:27 +0300

    Solved first problem by moving all zeroes to the end of the array while maintaining the order of non-zero elements

---
+++ problemOne.cs
@@ -0,0 +1,22 @@
+﻿public class problemOne
+{
+    public static void AllZeroToLift(int[] numbers)
+    {
+
+        int counter = 0;
+
+        foreach (var i in numbers)
+        {
+
+            if (i != 0)
+            {
+                numbers[counter++] = i;
+            }
+
+        }
+
+        for (int i = counter; i < numbers.Length; i++)
+                                  numbers[counter++] = 0;
+       
+    }
+}
